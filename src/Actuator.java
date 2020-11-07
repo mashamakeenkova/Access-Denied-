4142652f9ae8f0de2e9ec9ae95cfaf3d56b0df16
pfirlejczyk <74097440+pfirlejczyk@users.noreply.github.com>
2020-11-07 16:47:38 +0100

    Update Actuator.java

--- src/Actuator.java
+++ src/Actuator.java
@@ -2,8 +2,23 @@
 public class Actuator {
 	String name;
 	String state;
+	boolean someBool;
+	double someDouble;
+	int someInt;
 	
-	Actuator(String names){
+	Actuator(String names, int someInt){
+		name = names;		
+		this.someInt = someInt;
+		this.type = "int";
+	}
+	Actuator(String names, boolean someBool){		
 		name = names;
+		this.someBool = someBool;
+		this.type = "boolean";
+	}
+	Actuator(String names, double someDouble){
+		name = names;
+		this.someDouble = someDouble;
+		this.type = "double";
 	}
 }
