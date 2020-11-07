40d1c1739d7d1b609980400f4b45e7348d55dc29
alice <laporta.alice@gmail.com>
2020-11-07 15:24:07 +0100

    reads lines

--- src/Simulation.java
+++ src/Simulation.java
@@ -6,16 +6,24 @@ public class Simulation {
 	public static void main(String[] args) throws FileNotFoundException {
 		
 	Scanner scan = new Scanner(new File("Sensors_input.txt"));
+	
+	while (scan.hasNextLine()) {
 	String line = scan.nextLine();
     Scanner lineScan = new Scanner(line);
     String identity = lineScan.next();
-    //System.out.println(identity);
     identity = identity.toLowerCase();
-    System.out.println(identity);
     
-    if(identity == "sensor") {
+    
+    if(identity.equals("sensor")) {
     	System.out.println(identity);
     }
+    else if(identity.equals("actuator")) {
+    	System.out.println(identity);
+    }
+    else {
+    	System.out.println("make sure all your devices are either sensors or actuators and try again.");
+    }
     
+	}
 	}
 }
