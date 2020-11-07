5b71b71e4f3ebf5088ca618942917b5851118211
alice <laporta.alice@gmail.com>
2020-11-07 15:09:00 +0100

    simulation

--- src/Simulation.java
+++ src/Simulation.java
@@ -6,5 +6,16 @@ public class Simulation {
 	public static void main(String[] args) throws FileNotFoundException {
 		
 	Scanner scan = new Scanner(new File("Sensors_input.txt"));
+	String line = scan.nextLine();
+    Scanner lineScan = new Scanner(line);
+    String identity = lineScan.next();
+    //System.out.println(identity);
+    identity = identity.toLowerCase();
+    System.out.println(identity);
+    
+    if(identity == "sensor") {
+    	System.out.println(identity);
+    }
+    
 	}
 }
